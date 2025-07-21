e0d0c612cf3d5107e23178d0e1f66e9cc32c2ebc
SamB234 <43220294+SamB234@users.noreply.github.com>
2025-07-21 21:46:35 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-8 py-4 bg-gray-100 shadow-sm">
+
+    
+ /*   <nav className="flex items-center justify-between px-8 py-4 bg-gray-100 shadow-sm">
       {/* Logo */}
       <div className="text-xl font-semibold text-blue-700">ItsJustAMap</div>
 
@@ -20,5 +22,34 @@ export default function Navbar() {
         ))}
       </div>
     </nav>
+*/
+
+        
+     <nav className="backdrop-blur bg-gray-100 bg-opacity-60 fixed top-0 left-0 w-full z-50 shadow-sm flex items-center justify-between px-8 h-14 font-semibold text-blue-700">
+  <div className="text-xl font-bold">ItsJustAMap</div>
+
+  <div className="flex gap-6">
+    <a
+      href="#"
+      className="px-4 py-1 rounded-full border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-gray-100 transition"
+    >
+      Features
+    </a>
+    <a
+      href="#"
+      className="px-4 py-1 rounded-full border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-gray-100 transition"
+    >
+      Pricing
+    </a>
+    <a
+      href="#"
+      className="px-4 py-1 rounded-full border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-gray-100 transition"
+    >
+      Login / Sign Up
+    </a>
+  </div>
+</nav>
+   
+
   )
 }
