47ec9c65427a2f18b45f46f83abf01e94c983a38
SamB234 <43220294+SamB234@users.noreply.github.com>
2025-07-21 20:48:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Map from './components/Map'
 
+/*
 export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
        <h1>Hello from itsjustamap!</h1>
-      <Map />
+    </Map />
+    </div>
+  )
+}
+*/
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <h1>Hello from itsjustamap!</h1>
+      {/* <Map /> */}
     </div>
   )
 }
