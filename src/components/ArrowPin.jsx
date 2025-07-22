ce5c4dd912d033291cecc1f1d5977a9e3ab95f28
SamB234 <43220294+SamB234@users.noreply.github.com>
2025-07-22 20:36:45 +0100

    Update ArrowPin.jsx

--- src/components/ArrowPin.jsx
+++ src/components/ArrowPin.jsx
@@ -1,23 +1,85 @@
 import React from 'react'
 
 export default function ArrowPin({ onArrowClick }) {
-  const arrow = (dir, rotate, pos) => (
-    <div
-      onClick={() => onArrowClick(dir)}
-      className={`absolute ${pos} transform ${rotate} w-6 h-6 text-blue-700 hover:text-blue-900 cursor-pointer transition`}
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 19V5m0 0l-7 7m7-7l7 7" />
-      </svg>
-    </div>
-  )
+  // onArrowClick(direction) - callback when arrow clicked
+
+  const arrowClass = "w-6 h-6 text-blue-700 hover:text-blue-900 cursor-pointer transition"
 
   return (
-    <div className="relative w-16 h-16">
-      {arrow('N', '', 'top-0 left-1/2 -translate-x-1/2')}
-      {arrow('S', 'rotate-180', 'bottom-0 left-1/2 -translate-x-1/2')}
-      {arrow('E', 'rotate-90', 'top-1/2 right-0 -translate-y-1/2')}
-      {arrow('W', '-rotate-90', 'top-1/2 left-0 -translate-y-1/2')}
+    <div className="relative flex flex-col items-center">
+      {/* Central Pin */}
+      <div className="w-8 h-8 bg-blue-700 rounded-full flex items-center justify-center text-white font-bold select-none cursor-pointer">
+        📍
+      </div>
+
+      {/* Arrows */}
+      {/* North */}
+      <div
+        onClick={() => onArrowClick('N')}
+        className="absolute -top-8 left-1/2 transform -translate-x-1/2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          viewBox="0 0 24 24"
+          className={arrowClass}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 19V5m0 0l-7 7m7-7l7 7" />
+        </svg>
+      </div>
+
+      {/* South */}
+      <div
+        onClick={() => onArrowClick('S')}
+        className="absolute -bottom-8 left-1/2 transform -translate-x-1/2 rotate-180"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          viewBox="0 0 24 24"
+          className={arrowClass}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 19V5m0 0l-7 7m7-7l7 7" />
+        </svg>
+      </div>
+
+      {/* West */}
+      <div
+        onClick={() => onArrowClick('W')}
+        className="absolute top-1/2 -left-8 transform -translate-y-1/2 -rotate-90"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          viewBox="0 0 24 24"
+          className={arrowClass}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 19V5m0 0l-7 7m7-7l7 7" />
+        </svg>
+      </div>
+
+      {/* East */}
+      <div
+        onClick={() => onArrowClick('E')}
+        className="absolute top-1/2 -right-8 transform -translate-y-1/2 rotate-90"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          viewBox="0 0 24 24"
+          className={arrowClass}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 19V5m0 0l-7 7m7-7l7 7" />
+        </svg>
+      </div>
     </div>
   )
 }
